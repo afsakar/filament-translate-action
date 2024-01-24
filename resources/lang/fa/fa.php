3c1e366fc05638b953f8feaf2ba9b9eddd29b590
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-01-24 09:59:53 +0330

    Create fa.php
    add persian lang

---
+++ resources/lang/fa/fa.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'translate' => 'ترجمه',
+    'modal_title' => 'ترجمه با مترجم گوگل',
+    'source' => 'زبان مبدا',
+    'target' => 'زبان مقصد',
+    'success_title' => 'موفقیت آمیز!',
+    'success_message' => 'محتوا با موفقیت ترجمه شد.',
+    'error_title' => 'خطا!',
+    'error_message' => 'محتوا قابل ترجمه نمی باشد.',
+];
