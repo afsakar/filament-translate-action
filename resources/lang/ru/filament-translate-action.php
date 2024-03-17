5ead6e6e7caab8cf811e79a8003e64a614e30105
Arthur Boyko <homam600i@gmail.com>
2024-03-17 20:37:01 +0200

    RU & UA Language added

---
+++ resources/lang/ru/filament-translate-action.php
@@ -0,0 +1,13 @@
+<?php
+
+// translations for Afsakar/FilamentTranslateAction
+return [
+    'translate' => 'Перевод',
+    'modal_title' => 'Перевод через Google Translate',
+    'source' => 'Язык источника',
+    'target' => 'Язык перевода',
+    'success_title' => 'Переведено!',
+    'success_message' => 'Текст успешно переведен.',
+    'error_title' => 'Ошибка!',
+    'error_message' => 'Не удалось перевести текст.',
+];
---
+++ resources/lang/uk/filament-translate-action.php
@@ -0,0 +1,13 @@
+<?php
+
+// переклади для Afsakar/FilamentTranslateAction
+return [
+    'translate' => 'Переклад',
+    'modal_title' => 'Переклад через Google Translate',
+    'source' => 'Мова джерела',
+    'target' => 'Мова перекладу',
+    'success_title' => 'Перекладено!',
+    'success_message' => 'Текст успішно перекладено.',
+    'error_title' => 'Помилка!',
+    'error_message' => 'Не вдалося перекласти текст.',
+];
